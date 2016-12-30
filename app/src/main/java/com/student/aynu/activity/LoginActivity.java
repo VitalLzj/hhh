096c19ad161ad6c7f498f6afbd347684181afa16
VitalLzj <976623696@qq.com>
2016-12-30 11:50:12 +0800

    新增了提示

--- app/src/main/java/com/student/aynu/activity/LoginActivity.java
+++ app/src/main/java/com/student/aynu/activity/LoginActivity.java
@@ -12,7 +12,6 @@ import com.student.aynu.base.BaseActivity;
  */
 public class LoginActivity extends BaseActivity {
 
-    //新增提示
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
